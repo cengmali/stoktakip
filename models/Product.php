8a8aa2c7940301a3fb34e71e8681efce6f8452a5
Mehmet Ali Kaynarca <mehmetalikaynarca@gmail.com>
2016-11-27 16:23:35 +0300

    Db connection, model generation.

---
+++ models/Product.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "product".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property integer $type_id
+ *
+ * @property Type $type
+ */
+class Product extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'product';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['type_id'], 'integer'],
+            [['name'], 'string', 'max' => 255],
+            [['type_id'], 'exist', 'skipOnError' => true, 'targetClass' => Type::className(), 'targetAttribute' => ['type_id' => 'id']],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'No',
+            'name' => 'Ürün Adı',
+            'type_id' => 'Tip No',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getType()
+    {
+        return $this->hasOne(Type::className(), ['id' => 'type_id']);
+    }
+}
---
+++ models/Type.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "type".
+ *
+ * @property integer $id
+ * @property string $name
+ *
+ * @property Product[] $products
+ */
+class Type extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'type';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'No',
+            'name' => 'Tip Adı',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getProducts()
+    {
+        return $this->hasMany(Product::className(), ['type_id' => 'id']);
+    }
+}
